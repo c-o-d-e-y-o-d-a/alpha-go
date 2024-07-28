4b5f76c98cfb6c729d3cc72b5d0b710b7711a4f0
Madhav Gupta <e22cseu0827@bennett.edu.in>
2024-07-29 00:37:20 +0530

    feat: Complete profile page

--- lib/views/screens/profile_screen.dart
+++ lib/views/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:alpha_go/controllers/wallet_controller.dart';
+import 'package:buttons_tabbar/buttons_tabbar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -24,13 +25,15 @@ class _ProfilePageState extends State<ProfilePage> {
             height: 500,
             width: double.infinity,
             decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(20)),
+                color: Colors.grey.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(20)),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
                   CircleAvatar(
                     radius: 90,
+                    backgroundColor: Colors.grey,
                   ),
                   SizedBox(
                     height: 20,
@@ -86,6 +89,54 @@ class _ProfilePageState extends State<ProfilePage> {
                 ],
               ),
             ),
+          ),
+          Divider(
+            thickness: 2,
+          ),
+          Expanded(
+            child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DefaultTabController(
+                    length: 2,
+                    child: Column(
+                      children: <Widget>[
+                        ButtonsTabBar(
+                          backgroundColor: Colors.black.withOpacity(0.3),
+                          unselectedBackgroundColor:
+                              Colors.grey.withOpacity(0.2),
+                          borderWidth: 0,
+                          borderColor: Colors.black,
+                          labelStyle: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          unselectedLabelStyle: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          tabs: [
+                            Tab(
+                              text: "INSCRIPTIONS",
+                            ),
+                            Tab(
+                              text: "NFTS",
+                            ),
+                          ],
+                        ),
+                        Expanded(
+                          child: TabBarView(
+                            children: <Widget>[Text("Tab1"), Text("Tab2")],
+                            // Add your views here
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
           )
         ]),
       ),
