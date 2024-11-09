3a730a3cc377ca4944cecd00bbef75e9404aecff
Madhav Gupta <madhav.gupta7674@gmail.com>
2024-11-10 00:13:14 +0530

    feat: Location Puck and Model Layers added

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,12 +1,16 @@
+import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:image/image.dart' as img;
-import 'package:geolocator/geolocator.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart' as geo;
+import 'package:latlong2/latlong.dart' as ltlng;
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart' as mb;
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:searchfield/searchfield.dart';
 
@@ -18,7 +22,7 @@ class MapHomePage extends StatefulWidget {
 }
 
 class _MapHomePageState extends State<MapHomePage> {
-  LatLng? userPos;
+  ltlng.LatLng? userPos;
   Image? pointerImage;
   mb.MapboxMap? mapboxMap;
   final styleUrl = "mapbox://styles/powerclubglobal/cm2tx1qrp00fy01qw4oga0dqk";
@@ -37,38 +41,46 @@ class _MapHomePageState extends State<MapHomePage> {
     );
   }
 
-  Future<Position> determinePosition() async {
+  Future<geo.Position> determinePosition() async {
     bool serviceEnabled;
-    LocationPermission permission;
+    geo.LocationPermission permission;
 
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    serviceEnabled = await geo.Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       return Future.error('Location services are disabled.');
     }
 
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
+    permission = await geo.Geolocator.checkPermission();
+    if (permission == geo.LocationPermission.denied) {
+      permission = await geo.Geolocator.requestPermission();
+      if (permission == geo.LocationPermission.denied) {
         return Future.error('Location permissions are denied');
       }
     }
 
-    if (permission == LocationPermission.deniedForever) {
+    if (permission == geo.LocationPermission.deniedForever) {
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
 
     // When we reach here, permissions are granted and we can
     // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.best);
+    return await geo.Geolocator.getCurrentPosition(
+        desiredAccuracy: geo.LocationAccuracy.best);
+  }
+
+  Future<void> requestLocationPermission() async {
+    var status = await Permission.locationWhenInUse.status;
+    if (!status.isGranted) {
+      await Permission.location.request();
+    }
   }
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      await requestLocationPermission();
       mb.MapboxOptions.setAccessToken(apiKey);
 
       downloadImage(FirebaseAuth.instance.currentUser!.photoURL ??
@@ -78,23 +90,61 @@ class _MapHomePageState extends State<MapHomePage> {
           pointerImage = value;
         });
       });
-      mapboxMap?.location.updateSettings(mb.LocationComponentSettings(
-          locationPuck: mb.LocationPuck(
-              locationPuck3D: mb.LocationPuck3D(
-        modelUri:
-            "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF-Embedded/Duck.gltf",
-      ))));
-      // mapboxMap?.style.addLayer(mb.ModelLayer(id: id, sourceId: sourceId))
       determinePosition().then((value) {
         setState(() {
-          userPos = LatLng(value.latitude, value.longitude);
+          userPos = ltlng.LatLng(value.latitude, value.longitude);
         });
       });
+      //mapboxMap?.style.addLayer(mb.LocationIndicatorLayer(id: 'location-main'));
+
+      // mapboxMap?.style.addLayer(mb.ModelLayer(id: id, sourceId: sourceId))
     });
+  }
+
+  addModelLayer() async {
+    var value = mb.Point(
+        coordinates: mb.Position(76.85278280979402, 30.659204869650807));
+    if (mapboxMap == null) {
+      throw Exception("MapboxMap is not ready yet");
+    }
+
+    await mapboxMap?.style
+        .addSource(mb.GeoJsonSource(id: "sourceId", data: json.encode(value)));
+
+    final carModelId = "model-car-id";
+    final carModelUri =
+        "https://github.com/M4dhav/alpha-go/raw/dev/assets/rossiya_hotel/scene.gltf";
+    await mapboxMap?.style.addStyleModel(carModelId, carModelUri);
+
+    var modelLayer1 = ModelLayer(id: "modelLayer-car", sourceId: "sourceId");
+    modelLayer1.modelId = carModelId;
+    modelLayer1.modelScale = [5, 5, 5];
+    modelLayer1.modelRotation = [0, 0, 90];
+    modelLayer1.modelType = ModelType.COMMON_3D;
+    mapboxMap?.style.addLayer(modelLayer1);
   }
 
   _onMapCreated(mb.MapboxMap mapboxMap) {
     this.mapboxMap = mapboxMap;
+    mapboxMap.location.updateSettings(mb.LocationComponentSettings(
+        enabled: true,
+        puckBearing: mb.PuckBearing.HEADING,
+        puckBearingEnabled: true,
+        pulsingEnabled: true,
+        pulsingMaxRadius: 20,
+        locationPuck: mb.LocationPuck(
+            locationPuck3D: mb.LocationPuck3D(
+          modelUri:
+              "https://github.com/M4dhav/alpha-go/raw/dev/assets/man/scene.gltf",
+          modelScale: [70, 70, 70],
+          position: [userPos!.longitude, userPos!.latitude],
+        ))));
+    log('puck added');
+  }
+
+  _onStyleLoaded(StyleLoadedEventData data) async {
+    await addModelLayer();
+    log('style loaded');
   }
 
   @override
@@ -165,32 +215,33 @@ class _MapHomePageState extends State<MapHomePage> {
             : mb.MapWidget(
                 key: const ValueKey("mapWidget"),
                 onMapCreated: _onMapCreated,
+                onStyleLoadedListener: _onStyleLoaded,
                 styleUri: styleUrl,
                 cameraOptions: mb.CameraOptions(
-                    pitch: 90,
+                    pitch: 80,
                     center: mb.Point(
                         coordinates:
                             mb.Position(userPos!.longitude, userPos!.latitude)),
                     zoom: 12.0),
               ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            determinePosition().then((value) {
-              setState(() async {
-                await mapboxMap?.flyTo(
-                    mb.CameraOptions(
-                        pitch: 90,
-                        center: mb.Point(
-                            coordinates:
-                                mb.Position(value.longitude, value.latitude)),
-                        zoom: 12.0),
-                    mb.MapAnimationOptions());
-              });
-            });
-          },
-          child: const Icon(Icons.my_location),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {
+        //     determinePosition().then((value) {
+        //       setState(() async {
+        //         await mapboxMap?.flyTo(
+        //             mb.CameraOptions(
+        //                 pitch: 90,
+        //                 center: mb.Point(
+        //                     coordinates:
+        //                         mb.Position(value.longitude, value.latitude)),
+        //                 zoom: 12.0),
+        //             mb.MapAnimationOptions());
+        //       });
+        //     });
+        //   },
+        //   child: const Icon(Icons.my_location),
+        // ),
+        // floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
       ),
     );
   }
