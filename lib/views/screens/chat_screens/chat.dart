dd69b6d817350e7cc3b1d5b8047764d802814dd9
Madhav Gupta <madhav.gupta7674@gmail.com>
2025-05-17 02:36:05 +0530

    fix: Fixed chat screens

--- lib/views/screens/chat_screens/chat.dart
+++ lib/views/screens/chat_screens/chat.dart
@@ -1,12 +1,14 @@
 import 'dart:io';
+import 'dart:math';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:flutter_chat_core/flutter_chat_core.dart';
 import 'package:path_provider/path_provider.dart';
 
 class ChatPage extends StatefulWidget {
@@ -22,6 +24,7 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final chatController = InMemoryChatController();
   void _handleMessageTap(BuildContext _, types.Message message) async {
     if (message is types.FileMessage) {
       var localPath = message.uri;
@@ -109,35 +112,26 @@ class _ChatPageState extends State<ChatPage> {
                 initialData: const [],
                 stream: FirebaseChatCore.instance.messages(snapshot.data!),
                 builder: (context, snapshot) => Chat(
-                  messages: snapshot.data ?? [],
-                  onMessageTap: _handleMessageTap,
-                  onPreviewDataFetched: _handlePreviewDataFetched,
-                  onSendPressed: _handleSendPressed,
-                  showUserAvatars: true,
-                  user: types.User(
-                    id: FirebaseChatCore.instance.firebaseUser?.uid ?? '',
-                  ),
-                  theme: DefaultChatTheme(
-                    backgroundColor: Colors.transparent,
-                    primaryColor: Colors.black,
-                    secondaryColor: Colors.black,
-                    messageInsetsVertical: 10,
-                    messageInsetsHorizontal: 16,
-                    messageBorderRadius: 10,
-                    userAvatarNameColors: const [Colors.blue, Colors.red],
-                    userAvatarTextStyle: const TextStyle(color: Colors.white),
-                    receivedMessageBodyTextStyle:
-                        const TextStyle(color: Color(0xFFB4914B)),
-                    sentMessageBodyTextStyle:
-                        const TextStyle(color: Color(0xFFB4914B)),
-                    // receivedMessageBorderColor: Color(0xFFB4914B),
-                    // sentMessageBorderColor: Color(0xFFB4914B),
-                    inputTextColor: Colors.white,
-                    inputBackgroundColor: Colors.black,
-                    inputBorderRadius: BorderRadius.circular(10),
-                    inputTextStyle: const TextStyle(color: Colors.white),
-                    // inputButtonIcon: Icon(Icons.send, color: Color(0xFFB4914B)),
-                  ),
+                  currentUserId: FirebaseAuth.instance.currentUser!.uid,
+                  resolveUser: (String userId) async {
+                    await Future.delayed(const Duration(seconds: 1));
+                    return;
+                  },
+                  chatController: chatController,
+                  onMessageSend: (text) {
+                    chatController.insertMessage(
+                      TextMessage(
+                        // Better to use UUID or similar for the ID - IDs must be unique.
+                        id: '${Random().nextInt(1000) + 1}',
+                        authorId: 'user1',
+                        createdAt: DateTime.now().toUtc(),
+                        text: text,
+                      ),
+                    );
+                  },
+
+                  //messages: snapshot.data ?? [],
+                  //   onMessageTap: _handleMessageTap,
                 ),
               ),
             ),
